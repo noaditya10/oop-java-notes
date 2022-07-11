5265ea5c5ef69e1bf085e437fae5bb31b89a5d42
Novi Aditya <noaditya10@gmail.com>
2022-07-12 04:31:14 +0700

    super keyword : parent-child

---
+++ ShapeApp.java
@@ -0,0 +1,8 @@
+public class ShapeApp {
+    public static void main(String[] args) {
+        var rectangle = new Rectangle();
+
+        System.out.println(rectangle.getCorner());
+        System.out.println(rectangle.getParentCorner());
+    }
+}
