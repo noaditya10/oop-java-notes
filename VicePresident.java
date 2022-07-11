ea9e5df61be02373766bfef9df26203dde98bb44
Novi Aditya <noaditya10@gmail.com>
2022-07-12 04:26:47 +0700

    notes on method overriding

--- VicePresident.java
+++ VicePresident.java
@@ -3,4 +3,5 @@ public class VicePresident extends Manager{
     void sayHello(String name) {
         System.out.println("I am VP, " + name);
     }
+    //method overriding: nama, return, dan param harus sama
 }
