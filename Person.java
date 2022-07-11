b983e46311134b4ece5c4c29006b7ceb940d7dba
Novi Aditya <noaditya10@gmail.com>
2022-07-12 03:35:14 +0700

    init

---
+++ Person.java
@@ -0,0 +1,15 @@
+public class Person {
+    //fields/attributes/properties
+    String name;
+    String address;
+    final String country = "Indonesia";
+
+    Person(String paramName, String paramAddress){
+        name = paramName;
+        address = paramAddress;
+    }
+
+    void sayHello(String paramName) {
+        System.out.println("Hello, " + paramName + ". My name is " + name);
+    }
+}
---
+++ PersonApp.java
@@ -0,0 +1,10 @@
+public class PersonApp {
+
+    public static void main(String[] args) {
+        var person1 = new Person("Novi", "Babarsari");
+
+        System.out.println(person1.name);
+        person1.sayHello("Farhan");
+
+    }
+}
